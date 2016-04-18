b229b5f9a9c1593f2c5afbfbfc7eab943f2cd12d
JonaCodes <jonathanfarache@gmail.com>
2016-04-18 14:55:41 +0300

    add launch feature

--- rocket.js
+++ rocket.js
@@ -1,3 +1,9 @@
 var rocket = function(numMissiles){
 	console.log("Sacha rules give him" + numMissiles + "missiles");
+
+	this.launch = function(){
+		console.log("GOING TO ALPHA CENTAURI BRB")
+	}
 }
+
+module.exports = Rocket;
